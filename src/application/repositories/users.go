934a3e58dcc298f6d64874a3ff92ede66bd8bd01
AndreyArthur <twekylaywer@gmail.com>
2022-01-16 17:56:56 -0300

    feat: create a users repository interface

---
+++ src/application/repositories/users.go
@@ -0,0 +1,10 @@
+package repositories
+
+import "github.com/AndreyArthur/murao-oganessone/src/core/entities"
+
+type UsersRepository interface {
+	FindByUsername(username string, caseSensitive bool) (*entities.UserEntity, error)
+	FindByEmail(email string) (*entities.UserEntity, error)
+	Create(username string, email string) (*entities.UserEntity, error)
+	Save(*entities.UserEntity) error
+}
