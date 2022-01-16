25daf20b436d07394c8040226f1d6392b07b71c8
AndreyArthur <twekylaywer@gmail.com>
2022-01-16 18:09:47 -0300

    test: cover not found username case

---
+++ src/application/use-cases/create-user_test.go
@@ -0,0 +1,26 @@
+package usecases
+
+import (
+	"testing"
+
+	mock_repositories "github.com/AndreyArthur/murao-oganessone/src/application/repositories/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateUserUseCase_NotFoundByUsername(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	repo := mock_repositories.NewMockUsersRepository(ctrl)
+	createUserUseCase, _ := NewCreateUserUseCase(repo)
+	username, email, password := "username", "user@email.com", "p4ssword"
+	repo.EXPECT().
+		FindByUsername(username, true).
+		Return(nil, nil)
+
+	user, err := createUserUseCase.Execute(username, email, password)
+
+	assert.Nil(t, err)
+	assert.Equal(t, user.Username, username)
+	assert.Equal(t, user.Email, email)
+}
