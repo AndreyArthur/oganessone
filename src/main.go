ae10c2022607ca46149b54c4920418c78ee61866
AndreyArthur <twekylaywer@gmail.com>
2022-02-01 08:27:25 -0300

    feat: run on main

--- src/main.go
+++ src/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"github.com/AndreyArthur/oganessone/src/infrastructure/grpc"
+	google_grpc "google.golang.org/grpc"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	googleGrpcServer := google_grpc.NewServer()
+	server, err := grpc.NewGrpcServer(googleGrpcServer)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	listener, goerr := net.Listen("tcp", "0.0.0.0:50051")
+	if goerr != nil {
+		log.Fatal(err)
+		return
+	}
+	server.Start(listener)
 }
